20c66ac090b46d983693271f9c0e8cae2f5ba2e4
Srinu1239 <maguluri.srinu.1239@gmail.com>
2022-07-24 20:37:12 +0200

    Adding a storage stack

---
+++ stacks/StorageStack.js
@@ -0,0 +1,16 @@
+import { Table,Bucket } from "@serverless-stack/resources";
+
+export function StorageStack ({stack, app}) {
+    // Create an S3 bucket
+    const bucket = new Bucket(stack, "Uploads");
+    const table = new Table(stack, "Notes", {
+        fields: {
+            userId: "string",
+            noteId: "string"
+        },
+        primaryIndex: { partitionKey: "userId", sortKey: "noteId"},
+    });
+    return {
+        table,bucket,
+    };
+}
