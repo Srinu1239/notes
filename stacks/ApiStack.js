efc67c9e28f29c7de58f6b9305ae1fc5e01c68ca
Srinu1239 <maguluri.srinu.1239@gmail.com>
2022-07-25 12:59:38 +0200

    securing the Api

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -1,12 +1,14 @@
 import { Api,use } from "@serverless-stack/resources";
 import { StorageStack } from "./StorageStack";
 
+
 export function ApiStack({stack,app}) {
     const {table} = use(StorageStack);
 
     //create Api
     const api = new Api(stack, "Api", {
         defaults: {
+            authorizer: "iam",
             function: {
                 permissions: [table],
                 environment: {
